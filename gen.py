24eff9ea39b33cf2ba67cb705f50f6cd2cc48c0e
Антон <88902421+BouFFaNTCRiB@users.noreply.github.com>
2023-05-24 22:33:10 +0300

    Добавил основные файлы рабочего проекта

---
+++ gen.py
@@ -0,0 +1,42 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+import sys
+import gc
+
+if __name__ == "__main__":
+    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if len(sys.argv) < 4:
+        print('wrong arg count')
+        exit(1)
+    inp = sys.argv[1]
+    out = sys.argv[2]
+    print(inp)
+    if not inp.endswith('.xes'):
+        print('wrong file type')
+        exit(1)
+    if not out.endswith('.xes'):
+        print('wrong file type')
+        exit(1)
+
+    text = open(inp, 'r').read()
+    text = text.split('</log>', 1)[0]
+    model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-350M-mono").to(DEVICE)
+    tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-350M-mono", use_fast=False)
+    input_ids = tokenizer(text, return_tensors="pt").input_ids.to(DEVICE)
+    torch.cuda.empty_cache()
+    gc.collect()
+    generated_ids = model.generate(input_ids, max_new_tokens=int(sys.argv[3]))
+    output = open('out.xes', 'w')
+    i = 0
+    line = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    line_e = line.replace("\n", "")
+    lines = line_e.split('>')
+    last = 0
+    for lin in lines:
+        if '</event' in lin:
+            last = i
+        i += 1
+    for i in range(0, last):
+        output.write(f"{lines[i]}>\n")
+    output.write(f"\t</trace>\n")
+    output.write(f"</log>\n")
---
+++ openAI.py
@@ -0,0 +1,29 @@
+import os
+import openai
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print('wrong arg count')
+        exit(1)
+    inp = sys.argv[1]
+    out = sys.argv[2]
+    print(inp)
+    if not inp.endswith('.xes'):
+        print('wrong file type')
+        exit(1)
+    if not out.endswith('.xes'):
+        print('wrong file type')
+        exit(1)
+    text = open(inp, 'r').read()
+    openai.api_key = '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'
+
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=text,
+      max_tokens=int(sys.argv[3]),
+      temperature=0
+    )
+    output = open('out.xes', 'w')
+    for line in response:
+        output.write(f"{line}>\n")
