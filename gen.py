89db20b76ddb77a3ac8a24d4d997735fa9c4ac25
Антон <88902421+BouFFaNTCRiB@users.noreply.github.com>
2023-05-29 18:23:41 +0300

    Update gen.py

--- gen.py
+++ gen.py
@@ -4,6 +4,7 @@ import sys
 import gc
 
 if __name__ == "__main__":
+    # устанавливаем девайс обработки - если есть cuda и видеокарта, то gpu, иначе - cpu
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     if len(sys.argv) < 4:
         print('wrong arg count')
@@ -17,15 +18,19 @@ if __name__ == "__main__":
     if not out.endswith('.xes'):
         print('wrong file type')
         exit(1)
-
+    # обрабатываем входные данные
     text = open(inp, 'r').read()
     text = text.split('</log>', 1)[0]
+    # инициализируем модель и токенизатор
     model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-350M-mono").to(DEVICE)
     tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-350M-mono", use_fast=False)
+    # обрабатываем входные данные
     input_ids = tokenizer(text, return_tensors="pt").input_ids.to(DEVICE)
+    # генерируем
     torch.cuda.empty_cache()
     gc.collect()
     generated_ids = model.generate(input_ids, max_new_tokens=int(sys.argv[3]))
+    # выводим полученные данные в файл
     output = open('out.xes', 'w')
     i = 0
     line = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
